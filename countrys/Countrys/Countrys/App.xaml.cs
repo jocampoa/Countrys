a993961d7cabb06b0b7a61c565317bb32d5e7611
Juan Ocampo <david.ocampo.3928@gmail.com>
2018-04-08 00:11:06 -0500

    Change TargetFrameworkVersion

--- countrys/Countrys/Countrys/App.xaml.cs
+++ countrys/Countrys/Countrys/App.xaml.cs
@@ -9,25 +9,27 @@
     {
         #region Properties
         public static NavigationPage Navigator { get; internal set; }
+        public static MasterPage Master { get; internal set; }
         #endregion
 
         #region Constructors
         public App()
         {
             InitializeComponent();
+            this.MainPage = new MasterPage();
 
-            if (string.IsNullOrEmpty(Settings.Token))
-            {
-                this.MainPage = new NavigationPage(new LoginPage());
-            }
-            else
-            {
-                var mainViewModel = MainViewModel.GetInstance();
-                mainViewModel.Token = Settings.Token;
-                mainViewModel.TokenType = Settings.TokenType;
-                mainViewModel.Countrys = new CountrysViewModel();
-                Application.Current.MainPage = new MasterPage();
-            }
+            //if (string.IsNullOrEmpty(Settings.Token))
+            //{
+            //    this.MainPage = new NavigationPage(new LoginPage());
+            //}
+            //else
+            //{
+            //    var mainViewModel = MainViewModel.GetInstance();
+            //    mainViewModel.Token = Settings.Token;
+            //    mainViewModel.TokenType = Settings.TokenType;
+            //    mainViewModel.Countrys = new CountrysViewModel();
+            //    Application.Current.MainPage = new MasterPage();
+            //}
         }
         #endregion
 
